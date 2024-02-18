7fa7c44e969ebfd927ebc6bd3a2dde619ba0a096
Jose De La Cruz <jldlcv@gmail.com>
2024-02-18 11:28:48 +0100

    Changed leave_sys function

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-# Python program to check if the input number is odd or even. A number is even if 
-# division by 2 gives a remainder of 0, if the remainder is 1, it is an odd number.
+# NOTA: A number is even if division by 2 gives a remainder of 0, if the remainder is 1, it is an odd number.
 
 import os
 
 
 def leave_sys():
-	leave = input('¿Desea continuar? (S / N): ').lower()
-	if leave == 's':
-		main()
-	else:
-		os._exit(0)
+	while True:
+		leave = input('¿Desea continuar? (S / N): ').lower()
+		if leave == 's':
+			main()
+		elif leave == 'n':
+			os._exit(0)
+		else:
+			print('Entrada no válida')
 
 
 def get_number():
